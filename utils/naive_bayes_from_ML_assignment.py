3642ae310191af289ccb99aae36df45aeaa8fce0
Ibnu Sofian Firdaus <ibnu.cs2016@gmail.com>
2019-04-18 12:27:33 +0700

    Add extra classification

--- utils/naive_bayes_from_ML_assignment.py
+++ utils/naive_bayes_from_ML_assignment.py
@@ -6,7 +6,7 @@ Created on Thu Apr 18 11:02:02 2019
 """
 
 import pandas as pd
-from utils.naive_bayes import NaiveBayes
+from naive_bayes import NaiveBayes
 from sklearn.metrics import accuracy_score
 
 # load training data
@@ -29,4 +29,38 @@ y_test = data_test.sentiment
 result = nb.predict(X_test)
 
 # measure accuracy
-print(accuracy_score(y_test, result))
+print("Single label accuracy score :",accuracy_score(y_test, result))
+
+labels = []
+for n in range(len(X_test)):
+    items = []
+    for item in nb.calc_prob(X_test[n]):
+        items.append(item[0])
+    labels.append(items)
+
+for x in range(13):
+    count = 0
+    for y in range(len(y_test)):
+        cust_labels = labels[y][0:x+1]
+        if y_test[y] in cust_labels:
+            count += 1
+    score = count/len(y_test)
+    print("Multi labels ({}) accuracy score : {}".format(x+1, score))
+
+# result
+"""
+Single label accuracy score : 0.2277580071174377
+Multi labels (1) accuracy score : 0.2277580071174377
+Multi labels (2) accuracy score : 0.385008896797153
+Multi labels (3) accuracy score : 0.5269128113879004
+Multi labels (4) accuracy score : 0.6383451957295374
+Multi labels (5) accuracy score : 0.7333185053380783
+Multi labels (6) accuracy score : 0.8040480427046264
+Multi labels (7) accuracy score : 0.8580960854092526
+Multi labels (8) accuracy score : 0.9036921708185054
+Multi labels (9) accuracy score : 0.9403914590747331
+Multi labels (10) accuracy score : 0.9715302491103203
+Multi labels (11) accuracy score : 0.9933274021352313
+Multi labels (12) accuracy score : 0.9986654804270463
+Multi labels (13) accuracy score : 1.0
+"""
