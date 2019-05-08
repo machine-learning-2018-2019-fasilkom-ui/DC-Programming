8afedae304726aaca3aa67abb2508895a7d1fc47
Ilham Darmawan CP <ilhamdcp@gmail.com>
2019-05-08 12:24:06 +0700

    add form validation

--- mainSentiment/views.py
+++ mainSentiment/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
+import json
+
 
 # Create your views here.
 def index(request):
     response = {}
     if request.method == 'POST':
-        return HttpResponseRedirect('/')
-    return render(request, 'index.html', response)
+        response['chart_data'] = json.dumps([
+            {'y': 9, 'label': "sleep time"},
+            {'y': 30, 'label': "work time"},
+        ])
+        return render(request, 'index.html', response)
+    else:
+        return render(request, 'index.html', {'chart_data': []})
